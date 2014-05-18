ca136347b301ca89000506a67b71535e9d26632e
akshatmehrotra <akshatmehrotra@gmail.com>
2014-05-17 20:20:23 -0500

    completed CRUD

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,49 @@
 class PicturesController < ApplicationController
-  def show
+  def index
+    @list_of_pictures = Picture.all
+
   end
+
+  def show
+    # Your code goes here
+    # Use the information after the slash in the URL to look up the director with the corresponding ID number
+    # Store the director in the @director instance variable so the view can format it
+
+    @picture = Picture.find_by({ :id => params[:id] })
+  end
+
+  def create
+    caption = params[:caption]
+    source = params[:source]
+
+    @new_picture = Picture.new
+    @new_picture.caption = caption
+    @new_picture.source = source
+    @new_picture.save
+    redirect_to("/all_pictures")
+  end
+
+  def destroy
+    @picture = Picture.find_by({ :id => params[:id] })
+    @picture.destroy
+    redirect_to("/all_pictures")
+  end
+
+  def edit
+    @picture = Picture.find_by({:id => params[:id]})
+  end
+
+  def update
+
+    caption = params[:caption]
+    source = params[:source]
+
+    @updated_picture = Picture.find_by({:id=> params[:id]})
+    @updated_picture.caption = caption
+    @updated_picture.source = source
+    @updated_picture.save
+    redirect_to("/all_pictures")
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rxngif::Application.routes.draw do
+
+  # read
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  get("/all_pictures", { :controller => "pictures", :action => "index" })
+
+  #create
+  get("/new_picture_form", { :controller => "pictures", :action => "new" })
+  get("/create_picture", { :controller => "pictures", :action => "create" })
+
+  #destroy
+  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+
+
+  #edit
+  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
+
+
+
 end
